a9d07b64679745dc6fad5cfa0ab5b00e138ad606
Onni Viljamaa <onniviljamaa@Onnis-MacBook-Pro.local>
2025-04-25 20:46:31 +0300

    Build number to 14

--- app.config.js
+++ app.config.js
@@ -27,7 +27,7 @@ export default {
     },
     android: {
       package: "com.ballerai.app",
-      versionCode: 13,
+      versionCode: 14,
       googleServicesFile: "./google-services.json",
       adaptiveIcon: {
         foregroundImage: "./assets/images/adaptive-icon.png",
@@ -41,7 +41,7 @@ export default {
     },
     ios: {
       bundleIdentifier: "com.ballerbizoy.ballerai",
-      buildNumber: "13",
+      buildNumber: "14",
       googleServicesFile: "./GoogleService-Info.plist",
       supportsTablet: true,
       infoPlist: {
